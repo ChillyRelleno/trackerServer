a699ecd94b3b5f179905009d0b3417c99cd59bdc
Phillip Daw <phillipdaw@yahoo.com>
2017-09-18 21:01:30 +0000

    serves files from base directory with access-control-allow-origin header

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
+app.use(express.static(__dirname));
+
+
+app.listen(3000, function () {
+  console.log('CORS-enabled web server listening on port 3000')
+})
+

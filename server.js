86eebc9c46f1565c186e9d67d797761b4b5d7ef9
Phillip Daw <phillipdaw@yahoo.com>
2017-11-19 10:16:30 +0000

    aqi data refreshed from test source, saved with bounding boxes. working on saving colors

--- server.js
+++ server.js
@@ -10,24 +10,25 @@ var toGeoJSON  = require('./lib/togeojson.js')
 var GeoJSON = require('geojson')
 //var stringify = require('json-stringify')
 const util = require('util')
+
+
+//**************Functions shared between Fire / AQI*******************************//
 var DOWNLOAD_DIR = './dataCache/'
- var FIREREPLY = 'Updated fire perimeter data'
 
-var fireCache;
 
-var modifyFeatures = function(json) {
+var modifyFeatures = function(json, setStyleFunc, destCache) {
   var i = 0;
   fireCache = null
   //console.log(json)
   for (i = 0, len = json.features.length; i < len; i++) {
     //set style
-    json.features[i] = setFireStyle(json.features[i])
+    json.features[i] = setStyleFunc(json.features[i])  // setFireStyle(json.features[i])
     //Calculate and store bounds
     json.features[i] = calcBounds(json.features[i])
   }//for each feature
-  fireCache = json
+  destCache = json
   console.log("Bounding boxes calculated and stored for " + i + " perimeters")
-  return json
+   return json
 }//modifyFeatures()
 
 //Set Defaults for fire perimeters
@@ -40,36 +41,133 @@ var setFireStyle = function(feature) {
 
 //Calculate bounding boxes - GeoBounds
 var calcBounds = function(feature) { //json) {
-  //console.log(JSON.stringify(json))
-  //fireCache = null;
-  //store boundingBox as property in each feature
-  //var i = 0;
-  //for (i = 0, len = json.features.length; i < len; i++) {
-    //json.features[i].properties.color = "red"
-    //console.log(i)
-    //var feature = json.features[i]
     var extent = GeoBounds.extent(feature.geometry)
     feature.properties.extent = [
 	Number(extent[0]).toFixed(4), Number(extent[1]).toFixed(4),
 	Number(extent[2]).toFixed(4), Number(extent[3]).toFixed(4)]//extent//Number(extent).toFixed(4)
-  //}//for
-  //fireCache = json
   return feature
 }//calcBounds
 
-//prep fire data for calcBounds - MOVED TO FETCH PROMISE CHAIN
-//var prepFireDataForBounds = function(filePath) {}
 
-//fire setup
+//************AQI Functions****************************//
+
+//AQI variables
+var AQIREPLY = 'Updated air quality data'
+var aqiCache;
+var aqiUrl = "http://phillipdaw.com:3000/testAqicontUS.kml"//testAqi.kml"
+//contentintal US 
+//http://www.airnowapi.org/aq/kml/Combined/?DATE=2017-09-18T06&BBOX=-124.78,24.74,-66.95,49.35&SRS=EPSG:4326&API_KEY=8B8927D2-B8C3-4371-8E5D-902C4A129469
+
+
+//AQI Routes
+app.get('updateAqiData', (req, res) => {updateAqiData(req, res)})
+app.get('filter/aqi/:west/:south/:east/:north', function (req, res) {
+
+})
+
+var updateAqiData = function(req, res) {
+ console.log('updating air quality data')
+
+ var aqidest = DOWNLOAD_DIR + "AirQuality.json"
+ var aqimoddest = DOWNLOAD_DIR + "ModifiedAirQuality.json"
+
+  fetch(aqiUrl)
+    .then(function(res) {
+      console.log(AQIREPLY)
+        //prepFireDataForBounds(dest)
+        return res;
+    })
+    .then(function(res) { return res.text() })
+    .then(function(str) { return (new xmldom()).parseFromString(str, "text/xml") })
+    .then(function(xml) { return toGeoJSON.kml(xml) })
+    .then(function(json) { return aqiCache = modifyFeatures(json, setAqiStyle); })
+    .then(function(json) { fs.writeFile(aqimoddest, JSON.stringify(aqiCache), function (err) {
+          if (err) return console.log(err);
+          else return json;
+        }) })
+}()//updateAqiData
+
+//This function is identical to the fire one except for the data source
+//delete this and make a filterData(source, west, south, east, north) function in shared funcs
+function filterAQIByBounds(west, south, east, north) {
+  var i = 0;
+  var toSend = [];
+  var len = aqiCache.features.length;
+
+  console.log('Filtering ' + len + ' elements')
+  //Build geojson header? Use Library?
+
+  var boundingBox =  [west, south, east, north]
+  // Push intersecting rectangles onto toSend array
+  for (i = 0, len = aqiCache.features.length; i < len; i++) {
+    if (intersectRect(boundingBox,
+                        aqiCache.features[i].properties.extent)) {
+        console.log("POLYGON " + i + " - TRUE")
+        toSend.push(aqiCache.features[i])
+    }//if intersect
+    else console.log("Polygon " + i + " - false")
+
+  }//for
+  return toSend;
+}//filterAqiByBounds
+
+var aqiJob = schedule.scheduleJob('* 30 /1 * * *', updateFireData);
+
+
+var setAqiStyle = function(feature) {
+  //feature.properties.color = "yellow"
+//  if (features.properties.fill !== undefined)
+//    feature.properties.color = feature.properties.fill
+  feature.properties.opacity = 0.5
+  //feature.properties.setProperty('fill-opacity', 0.5)//fillOpacity = 0.5
+  return feature
+}//setStyle()
+
+
+
+//************Fire Functions****************************//
+
+//Fire variables
+var FIREREPLY = 'Updated fire perimeter data'
+var fireCache;
 var fireUrl = "http://phillipdaw.com:3000/testFirePerimeters.kml"
 //"https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/ActiveFirePerimeters.kml";
 
+//Fire Default Style
+//Set Defaults for fire perimeters
+var setFireStyle = function(feature) {
+  feature.properties.color = "red"
+  feature.properties.opacity = 0.5
+  //feature.properties.setProperty('fill-opacity', 0.5)//fillOpacity = 0.5
+  return feature
+}//setStyle()
+
+
+//Fire Routes
+app.get('/updateFireData', (req, res) => {updateFireData(req, res)})
+app.get('/filter/fire/:west/:south/:east/:north', function (req, res) {
+  console.log('west : ' + req.params.west)
+  console.log('south : ' + req.params.south)
+  console.log('east : ' + req.params.east)
+  console.log('north : ' + req.params.north)
+
+  var matching = filterFireByBounds(req.params.west,
+         req.params.south, req.params.east, req.params.north)
+   console.log(util.inspect(matching,false,null))
+   console.log("Matching: " + matching./*features.*/length + ' elements')
+
+  if (typeof res !== "undefined") {
+    res.json(matching);
+  }//if reply needed
+})
+
+//Update data from server - currently just using my test data
 var updateFireData = function(req, res) {
-  console.log('updating...')
+  console.log('updating fire data...')
   //console.log(res)
   //todo replace with file name from res
-  var dest =  DOWNLOAD_DIR + "ActiveFirePerimeters.json"
-  var moddest = DOWNLOAD_DIR + "ModifiedFirePerimeters.json"
+  var firedest =  DOWNLOAD_DIR + "ActiveFirePerimeters.json"
+  var firemoddest = DOWNLOAD_DIR + "ModifiedFirePerimeters.json"
 
   fetch(fireUrl)
     .then(function(res) { 
@@ -82,31 +180,47 @@ var updateFireData = function(req, res) {
     .then(function(str) { return (new xmldom()).parseFromString(str, "text/xml") })
     .then(function(xml) { return toGeoJSON.kml(xml) })
     //.then(function(json) { console.log(JSON.stringify(json)); return json; })
-    .then(function(json) { return modifyFeatures(json); })//calcBounds(json); })
+    .then(function(json) { return fireCache = modifyFeatures(json, setFireStyle); })//calcBounds(json); })
     //.then(function(json) { console.log(JSON.stringify(json)); return json; })
-    .then(function(json) { fs.writeFile(moddest, JSON.stringify(fireCache), function (err) {
+    .then(function(json) { fs.writeFile(firemoddest, JSON.stringify(fireCache), function (err) {
           if (err) return console.log(err);
           else return json;
         }) })
-    //.then(function(json) { fs.writeFile(dest, JSON.stringify(json), function (err) {
-    //      if (err) return console.log(err);
-    //      else { return json; }
-    //    }) })
-
-//    .then(json => calcBounds(json))
 
   if (typeof res !== "undefined") res.sendStatus("<h1>" + FIREREPLY + "</h1>")
 }()//
 
+function filterFireByBounds(west, south, east, north) {
+  var i = 0;
+  var toSend = [];
+  var len = fireCache.features.length;
+
+  console.log('Filtering ' + len + ' elements')
+  //Build geojson header? Use Library?
+
+  var boundingBox =  [west, south, east, north]
+  // Push intersecting rectangles onto toSend array
+  for (i = 0, len = fireCache.features.length; i < len; i++) {
+    if (intersectRect(boundingBox,
+                        fireCache.features[i].properties.extent)) {
+        console.log("POLYGON " + i + " - TRUE")
+        toSend.push(fireCache.features[i])
+    }//if intersect
+    else console.log("Polygon " + i + " - false")
+
+  }//for
+  return toSend;
+}
+
+
+
 var fireJob = schedule.scheduleJob('* * /1 * * *', updateFireData);
-app.get('/updateFireData', (req, res) => {updateFireData(req, res)})
 
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-
     next();
 }
 
@@ -117,8 +231,6 @@ var LEFT = 0;
 var BOTTOM = 1;
 var RIGHT = 2;
 var TOP = 3
-
-
 function intersectRect(r1, r2) {
   //console.log(r1, r2)
   //console.log("r1L > r2R ? " + (+(r1[LEFT]) > +(r2[RIGHT]))) //they all fail this rule...
@@ -133,59 +245,7 @@ function intersectRect(r1, r2) {
   //console.log('')
   return intersect
 }
-//  return !(r2[LEFT] > r1[RIGHT] || 
-//           r2[RIGHT] < r1[LEFT] || 
-//           r2[TOP] > r1[BOTTOM] ||
-//           r2[BOTTOM] < r1[TOP]);
-//}
-function filterFireByBounds(west, south, east, north) {
-  var i = 0;
-  var toSend = [];
-  var len = fireCache.features.length;
 
-  console.log('Filtering ' + len + ' elements')
-  //Build geojson header? Use Library?
-
-  var boundingBox =  [west, south, east, north]
-		    //[Number(west).toFixed(4), Number(south).toFixed(4), 
-		    //	Number(east).toFixed(4), Number(north).toFixed(4)];
-  // Push intersecting rectangles onto toSend array
-  for (i = 0, len = fireCache.features.length; i < len; i++) {
-    //console.log(fireCache.features[i].properties.extent)
-    //console.log(boundingBox, 
-	//fireCache.features[i].properties.extent)
-    if (intersectRect(boundingBox, 
-			fireCache.features[i].properties.extent)) {
-	console.log("POLYGON " + i + " - TRUE")
-	toSend.push(fireCache.features[i])
-    }//if intersect 
-    else console.log("Polygon " + i + " - false")
-    
-  }//for
-  //console.log(toSend)
-  //var geojson = GeoJSON.parse(toSend, {Point: ['lat', 'lng', 'z'], 'Polygon': 'polygon'})
-//  console.log(util.inspect(geojson, false, null))//JSON.stringify(geojson))
-  return toSend;
-}
-
-app.get('/filter/fire/:west/:south/:east/:north', function (req, res) {
-  console.log('west : ' + req.params.west)
-  console.log('south : ' + req.params.south)
-  console.log('east : ' + req.params.east)
-  console.log('north : ' + req.params.north)
-  
-  var matching = filterFireByBounds(req.params.west,
-	 req.params.south, req.params.east, req.params.north)
-//  console.log(matching)
-   console.log(util.inspect(matching,false,null))
-   console.log("Matching: " + matching./*features.*/length + ' elements')
-   
-if (typeof res !== "undefined") {
-  //res.header('Content-Type', 'application/json');
-  //res.sendStatus(JSON.stringify(matching))
-   res.json(matching);
-  }//if reply needed
-})
 
 app.get('filter/aqi/:west/:south/:east/:north', function (req, res) {
 

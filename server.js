8d7540b38c433f8c038f5a3ad731663dcbb82660
Phillip Daw <phillipdaw@yahoo.com>
2017-11-21 23:58:34 +0000

    mostly cleanup

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('updateAqiData', (req, res) => {updateAqiData(req, res)})
 app.get('/filter/aqi/:west/:south/:east/:north', function (req, res) {
   var matching = filterAqiByBounds(req.params.west,
          req.params.south, req.params.east, req.params.north)
-   console.log(util.inspect(matching,false,null))
+   //console.log(util.inspect(matching,false,null))
    console.log("Matching: " + matching./*features.*/length + ' elements')
 
   if (typeof res !== "undefined") {
@@ -75,7 +75,7 @@ app.get('/filter/aqi/:west/:south/:east/:north', function (req, res) {
 
 
 var updateAqiData = function(req, res) {
- console.log('updating air quality data')
+ //console.log('updating air quality data')
 
  var aqidest = DOWNLOAD_DIR + "AirQuality.json"
  var aqimoddest = DOWNLOAD_DIR + "ModifiedAirQuality.json"
@@ -111,23 +111,20 @@ function filterAqiByBounds(west, south, east, north) {
   for (i = 0, len = aqiCache.features.length; i < len; i++) {
     if (intersectRect(boundingBox,
                         aqiCache.features[i].properties.extent)) {
-        console.log("POLYGON " + i + " - TRUE")
+    //    console.log("POLYGON " + i + " - TRUE")
         toSend.push(aqiCache.features[i])
     }//if intersect
-    else console.log("Polygon " + i + " - false")
+    //else console.log("Polygon " + i + " - false")
 
   }//for
   return toSend;
 }//filterAqiByBounds
 
-var aqiJob = schedule.scheduleJob('* 30 /1 * * *', updateFireData);
+var aqiJob = schedule.scheduleJob('* 30 /1 * * *', function() { updateAqiData() });
 
 
 var setAqiStyle = function(feature) {
-  //feature.properties.color = "yellow"
   if (typeof(feature.properties.fill) !== undefined) {
-//    if (features.properties.fill !== 1)
-//      console.log(feature
       feature.properties.color = feature.properties.fill
   }
   feature.properties.opacity = 0.5
@@ -142,7 +139,7 @@ var setAqiStyle = function(feature) {
 //Fire variables
 var FIREREPLY = 'Updated fire perimeter data'
 var fireCache;
-var fireJob = schedule.scheduleJob('* * /1 * * *', updateFireData);
+var fireJob = schedule.scheduleJob('* 15 /1 * * *', function () { updateFireData() });
 var fireUrl = "http://phillipdaw.com:3000/testFirePerimeters.kml"
 //"https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/ActiveFirePerimeters.kml";
 
@@ -159,14 +156,10 @@ var setFireStyle = function(feature) {
 //Fire Routes
 app.get('/updateFireData', (req, res) => {updateFireData(req, res)})
 app.get('/filter/fire/:west/:south/:east/:north', function (req, res) {
-  console.log('west : ' + req.params.west)
-  console.log('south : ' + req.params.south)
-  console.log('east : ' + req.params.east)
-  console.log('north : ' + req.params.north)
 
   var matching = filterFireByBounds(req.params.west,
          req.params.south, req.params.east, req.params.north)
-   console.log(util.inspect(matching,false,null))
+   //console.log(util.inspect(matching,false,null))
    console.log("Matching: " + matching./*features.*/length + ' elements')
 
   if (typeof res !== "undefined") {
@@ -176,8 +169,7 @@ app.get('/filter/fire/:west/:south/:east/:north', function (req, res) {
 
 //Update data from server - currently just using my test data
 var updateFireData = function(req, res) {
-  console.log('updating fire data...')
-  //console.log(res)
+  //console.log('updating fire data...')
   //todo replace with file name from res
   var firedest =  DOWNLOAD_DIR + "ActiveFirePerimeters.json"
   var firemoddest = DOWNLOAD_DIR + "ModifiedFirePerimeters.json"
@@ -216,10 +208,10 @@ function filterFireByBounds(west, south, east, north) {
   for (i = 0, len = fireCache.features.length; i < len; i++) {
     if (intersectRect(boundingBox,
                         fireCache.features[i].properties.extent)) {
-        console.log("POLYGON " + i + " - TRUE")
+        //console.log("POLYGON " + i + " - TRUE")
         toSend.push(fireCache.features[i])
     }//if intersect
-    else console.log("Polygon " + i + " - false")
+    //else console.log("Polygon " + i + " - false")
 
   }//for
   return toSend;
@@ -244,17 +236,10 @@ var BOTTOM = 1;
 var RIGHT = 2;
 var TOP = 3
 function intersectRect(r1, r2) {
-  //console.log(r1, r2)
-  //console.log("r1L > r2R ? " + (+(r1[LEFT]) > +(r2[RIGHT]))) //they all fail this rule...
-  //console.log("r1R < r2L ? " + (+(r1[RIGHT]) < +(r2[LEFT])))
-  //console.log("r1T < r2B ? " + (+(r1[TOP]) < +(r2[BOTTOM])))
-  //console.log("r1B > r2T ? " +  (+(r1[BOTTOM]) > +(r2[TOP])))
   var intersect = !((+(r1[LEFT]) > +(r2[RIGHT])) ||
           (+(r1[RIGHT]) < +(r2[LEFT])) ||
           (+(r1[TOP]) < +(r2[BOTTOM])) ||
           (+(r1[BOTTOM]) > +(r2[TOP])))
-  //console.log("Polygon " + intersect)
-  //console.log('')
   return intersect
 }
 
@@ -263,38 +248,4 @@ function intersectRect(r1, r2) {
 app.listen(3000, function () {
   console.log('CORS-enabled web server listening on port 3000')
 })
-
-//AQI setup
-//
-//var aqiApiKey = "8B8927D2-B8C3-4371-8E5D-902C4A129469"
-//var aqiUrl = "http://www.airnowapi.org/aq/kml/Combined/?SRS=EPSG:4326" +
-//              "&API_KEY=" + aqiApiKey;//to be continued in scheduled function
-//var aqiRule = new schedule.RecurrenceRule();
-//aqiRule.hour = 2;
-//var aqiJob = schedule.scheduleJob(aqiRule, function() {
-//  var today = new Date();
-//  fetch(aqiUrl
-//})
-
-// "http://www.airnowapi.org/aq/kml/Combined/?DATE=" + this.props.date.getFullYear() +
-//                  "-" + (this.props.date.getMonth()+1) + "-" + this.props.date.getDate() + "T06&BBOX=" +
-//                  west + "," + south + "," + east + "," + north +
-//                  "&SRS=EPSG:4326&API_KEY=" + this.props.apiKey;
-
-
-
-// Function to download file using wget
-//var download_file_wget = function(file_url) {
-//
-//    // extract the file name
-//   var file_name = url.parse(file_url).pathname.split('/').pop();
-//    // compose the wget command
-//    var wget = 'wget -P ' + DOWNLOAD_DIR + ' ' + file_url;
-//    // excute wget using child_process' exec function
-//
-//    var child = exec(wget, function(err, stdout, stderr) {
-//        if (err) throw err;
-//        else console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
-//    });
-//};
 

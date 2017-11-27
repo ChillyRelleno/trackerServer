f2046e5982ab8a5abebeb1f38c6acdb7ce01a71c
Phillip Daw <phillipdaw@yahoo.com>
2017-11-27 10:46:22 +0000

    working with both test and true data sources from update to filter to everything

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ getTestRoute = function(req, res)  {
      var geobuf = geojsonToGeobuf(json);
      //res.send(json);
      res.send(new Buffer(geobuf));
-  } );//readfile
+  } );//
 };//getTestRoute
 app.get('/getTestRoute', (req, res) => {getTestRoute(req, res);});
 
@@ -45,18 +45,21 @@ app.get('/getTestRoute', (req, res) => {getTestRoute(req, res);});
 //**************Functions shared between Fire / AQI*******************************//
 
 
-var modifyFeatures = function(json, setStyleFunc, destCache) {
+var modifyFeatures = function(json, setStyleFunc) {
   var i = 0;
-  fireCache = null
-  
+  //if (json.features[0].properties.type == "AQI")
+  //console.log(json);
   for (i = 0, len = json.features.length; i < len; i++) {
     //set style
     json.features[i] = setStyleFunc(json.features[i])  // setFireStyle(json.features[i])
+
     //Calculate and store bounds
     json.features[i] = calcBounds(json.features[i])
   }//for each feature
-  destCache = json
-  console.log("Bounding boxes calculated and stored for " + i + " perimeters")
+  //destCache = json
+  console.log("Bounding boxes calculated and stored for " + i +
+	" " +json.features[0].properties.type + " perimeters")
+  //console.log( destCache)
    return json
 }//modifyFeatures()
 
@@ -72,8 +75,8 @@ function sendFilteredResponse(matching, west, south, east, north, res) {
 }//sendFilteredResponse
 
 
-function filterFeaturesByBounds(cache, west, south, east, north, 
-		optPerFeatureFunc, optFCFunc) {
+filterFeaturesByBounds = (cache, west, south, east, north, 
+		optPerFeatureFunc, optFCFunc) => {
   var i = 0, toSend= [], len = cache.features.length;
   console.log('Filtering ' + len + ' elements');
   var boundingBox =  [Number(west), Number(south), Number(east), Number(north)]
@@ -125,76 +128,12 @@ var geobufToGeojson = function(geobuf) {
 
 //AQI variables
 var AQIREPLY = 'Updated air quality data'
-var aqiCache;
-var aqiUrl = "http://phillipdaw.com:" + config.serverPort +
-	"/testAqicontUS.kml"
-//contentintal US 
-//http://www.airnowapi.org/aq/kml/Combined/?DATE=2017-09-18T06&BBOX=-124.78,24.74,-66.95,49.35&SRS=EPSG:4326&API_KEY=8B8927D2-B8C3-4371-8E5D-902C4A129469
-
+var aqiLegend = [];
 
 function aqiDataPrep(toSend, boundingBox) {
         toSend = buildLegend(clipPolysByBounds(toSend, boundingBox));
 	return toSend;
 }//aqiDataPrep
-
-
-//AQI Routes
-//app.get('/updateAqiData', (req, res) => {updateAqiData(req, res)})
-	//function(req, res) { updateAqiData(req, res).bind(this); });
-	//(req, res) => {updateAqiData(req, res)})//.bind(this)})
-app.get('/filter/aqi/:west/:south/:east/:north', function (req, res) {
-  var matching = null;
-  matching = filterFeaturesByBounds(aqiCache, 
-	req.params.west, req.params.south, 
-	req.params.east, req.params.north,
-                checkLegend, aqiDataPrep);
-  sendFilteredResponse(matching,
-                req.params.west, req.params.south,
-                req.params.east, req.params.north,
-		res);
-})//filter aqi by bounds route
-
-
-var updateAqiData = function(req, res) {
-//updateAqiData = (req, res) => {
- //console.log('updating air quality data')
-
- var aqidest = DOWNLOAD_DIR + "AirQuality.json"
- var aqimoddest = DOWNLOAD_DIR + "ModifiedAirQuality.json"
-
-  fetch(aqiUrl)
-    .then(function(res) {
-      console.log(AQIREPLY)
-        return res;
-    })
-    .then(function(res) { return res.text() })
-    .then(function(str) { return (new xmldom()).parseFromString(str, "text/xml") })
-    .then(function(xml) { return toGeoJSON.kml(xml) })
-    .then(function(json) { return aqiCache = modifyFeatures(json, setAqiStyle); })
-    .then(function(json) { return buildLegend(json) })
-    .then(function(json) { fs.writeFile(aqimoddest, JSON.stringify(aqiCache), function (err) {
-          if (err) return console.log(err);
-          else return json;
-        }) })
-
- if (typeof res !== "undefined") res.sendStatus("<h1>" + AQIREPLY + "</h1>")
-
-}//updateAqiData
-updateAqiData();
-
-//var aqiJob = schedule.scheduleJob('* 30 /1 * * *', function() { this.updateAqiData() });
-
-var aqiLegend = [];
-var setAqiStyle = function(feature) { 
-  if (typeof(feature.properties.fill) !== undefined) {
-    feature.properties.color = feature.properties.fill;
-    feature.properties.opacity = 0.5
-    
-  }//if there's a color
-  feature.properties.type = "AQI";
-  //feature.properties.setProperty('fill-opacity', 0.5)//fillOpacity = 0.5
-  return feature
-}//setStyle()
 
 var checkLegend = function(feature) {
     var contains = false;
@@ -216,14 +155,124 @@ var buildLegend = function(fc) {
   return fc;//aqiLegend;
 }//buildLegend
 
+
+//AQI Routes
+//app.get('/updateAqiData', (req, res) => {updateAqiData(req, res)})
+	//function(req, res) { updateAqiData(req, res).bind(this); });
+	//(req, res) => {updateAqiData(req, res)})//.bind(this)})
+app.get('/filter/aqi/:west/:south/:east/:north', function (req, res) {
+  var matching = null;
+  matching = filterFeaturesByBounds(aqiCache, 
+	req.params.west, req.params.south, 
+	req.params.east, req.params.north,
+                checkLegend, aqiDataPrep);
+  sendFilteredResponse(matching,
+                req.params.west, req.params.south,
+                req.params.east, req.params.north,
+		res);
+})//filter aqi by bounds route
+
+
+app.get('/filter/fireSeason/aqi/:west/:south/:east/:north', function (req, res) {
+  var matching = null;
+  matching = filterFeaturesByBounds(aqiTestCache,
+        req.params.west, req.params.south,
+        req.params.east, req.params.north,
+                checkLegend, aqiDataPrep);
+  sendFilteredResponse(matching,
+                req.params.west, req.params.south,
+                req.params.east, req.params.north,
+                res);
+})//filter aqi by bounds route
+//Move to general functions
+var updateData = (url, callback, setStyleFunc, modifiedDest) => {
+  console.log("----"+url+"----")
+  fetch(url)
+    .then(function(res) { return res.text() })
+    .then(function(str) { return (new xmldom()).parseFromString(str, "text/xml") })
+    .then(function(xml) { return toGeoJSON.kml(xml) })
+    .then(function(json) { return callback( modifyFeatures(json, setStyleFunc)); })
+    //.then(function(json) { console.log(json);return json;})//"Length: "+ json.features.length);return json; })
+    .then(function(json) {
+        fs.writeFile(modifiedDest, JSON.stringify(json), function (err) {
+          if (err) return console.log(err);
+	  else return json;
+        }) })
+}
+
+//contentintal US 
+//"http://www.airnowapi.org/aq/kml/Combined/?DATE=2017-09-18T06
+//&BBOX=-124.78,24.74,-66.95,49.35&SRS=EPSG:4326&API_KEY="  +
+//8B8927D2-B8C3-4371-8E5D-902C4A129469
+
+var aqiCache;
+var aqiTestCache;
+var aqiTestUrl = "http://phillipdaw.com:" + config.serverPort +
+        "/testAqicontUS.kml";
+//contentintal US
+var aqiUrlPreDate = "http://www.airnowapi.org/aq/kml/Combined/?DATE="
+//2017-09-18
+var aqiUrlPostDate = "T06&BBOX=-124.78,24.74,-66.95,49.35&SRS=EPSG:4326&API_KEY=" + 
+		config.aqiApiKey;
+//8B8927D2-B8C3-4371-8E5D-902C4A129469
+
+var setAqiStyle = function(feature) {
+  if (typeof(feature.properties.fill) !== undefined) {
+    feature.properties.color = feature.properties.fill;
+
+  }//if there's a color
+  feature.properties.opacity = 0.5
+  feature.properties.type = "AQI";
+  //feature.properties.setProperty('fill-opacity', 0.5)//fillOpacity = 0.5
+  return feature
+}//setStyle()
+
+aqiCallback = (json) => {
+ aqiCache = json;
+}
+
+aqiTestCallback = (json) => {
+  aqiTestCache = json;
+}
+
+//updateAqiData
+//Updates both the test data (from scratch)and the live data from airnowapi.org
+//Meant to be run hourly
+//Saves to disk. Will make function to read from disk on startup
+var updateAqiData = function(req, res) {
+//updateAqiData = (req, res) => {
+ //console.log('updating air quality data')
+
+ var aqidest = DOWNLOAD_DIR + "AirQuality.json"
+ var aqimoddest = DOWNLOAD_DIR + "ModifiedAirQuality.json" 
+ var aqitestdest = DOWNLOAD_DIR + "TestAirQuality.json"
+ var aqitestmoddest = DOWNLOAD_DIR + "TestModifiedAirQuality.json"
+
+ var today = new Date();
+ var todayStr = String(today.getFullYear()) + "-" + String(Number(today.getMonth())+1) + "-" +
+	String(today.getDate());
+ //console.log(todayStr);
+ var aqiUrl = aqiUrlPreDate + todayStr + aqiUrlPostDate;
+//(url, cache, setStyleFunc, dest, modDest)
+ updateData(aqiUrl, aqiCallback, setAqiStyle, aqimoddest);
+ updateData(aqiTestUrl, aqiTestCallback, setAqiStyle, aqitestmoddest);
+
+ if (typeof res !== "undefined") res.sendStatus("<h1>" + AQIREPLY + "</h1>")
+
+}//updateAqiData
+updateAqiData();
+
+//var aqiJob = schedule.scheduleJob('* 30 /1 * * *', function() { this.updateAqiData() });
+
+
 //************Fire Functions****************************//
 
 //Fire variables
 var FIREREPLY = 'Updated fire perimeter data'
-var fireCache;
+var fireCache = null, fireTestCache = null;
 //var fireJob = schedule.scheduleJob('* 15 /1 * * *', function () { this.updateFireData() });
-var fireUrl = "http://phillipdaw.com:" + config.serverPort + "/testFirePerimeters.kml"
-//"https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/ActiveFirePerimeters.kml";
+var fireTestUrl = "http://phillipdaw.com:" + config.serverPort + "/testFirePerimeters.kml"
+var fireUrl = "https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/ActiveFirePerimeters.kml";
 
 //Fire Default Style
 //Set Defaults for fire perimeters
@@ -239,6 +288,7 @@ var setFireStyle = function(feature) {
 //Fire Routes
 //app.get('/updateFireData', (req, res) => {updateFireData(req, res)})
 app.get('/filter/fire/:west/:south/:east/:north', function (req, res) {
+  console.log(fireCache)
   var matching = filterFeaturesByBounds(fireCache,
                 req.params.west, req.params.south,
                 req.params.east, req.params.north)
@@ -248,14 +298,42 @@ app.get('/filter/fire/:west/:south/:east/:north', function (req, res) {
 		res);
 })//filter/fire
 
+
+app.get('/filter/fireSeason/fire/:west/:south/:east/:north', function (req, res) {
+  var matching = filterFeaturesByBounds(fireTestCache,
+                req.params.west, req.params.south,
+                req.params.east, req.params.north)
+  sendFilteredResponse({type: "FeatureCollection", features: matching},
+                req.params.west, req.params.south,
+                req.params.east, req.params.north,
+                res);
+})//filter/fireSeason/fire
+
+fireCallback = (json) => {
+ fireCache = json;
+}
+
+fireTestCallback = (json) => {
+  fireTestCache = json;
+}
+
+
 //Update data from server - currently just using my test data
 var updateFireData = function(req, res) {
   //console.log('updating fire data...')
   //todo replace with file name from res
   var firedest =  DOWNLOAD_DIR + "ActiveFirePerimeters.json"
   var firemoddest = DOWNLOAD_DIR + "ModifiedFirePerimeters.json"
-
-  fetch(fireUrl)
+  var firetestdest =  DOWNLOAD_DIR + "TestActiveFirePerimeters.json"
+  var firetestmoddest = DOWNLOAD_DIR + "TestModifiedFirePerimeters.json"
+//  fireCache = null;
+  //fireTestCache = null;
+ //updateData(aqiUrl, aqiCache, setAqiStyle, aqimoddest);
+ //updateData(aqiTestUrl, aqiTestCache, setAqiStyle, aqitestmoddest);
+  updateData(fireUrl, fireCallback, setFireStyle, firemoddest);
+//console.log(fireCache);
+  updateData(fireTestUrl, fireTestCallback, setFireStyle, firetestmoddest);
+  /*fetch(fireTestUrl)
     .then(function(res) { 
       console.log(FIREREPLY) 
 	return res;
@@ -263,11 +341,11 @@ var updateFireData = function(req, res) {
     .then(function(res) { return res.text() })
     .then(function(str) { return (new xmldom()).parseFromString(str, "text/xml") })
     .then(function(xml) { return toGeoJSON.kml(xml) })
-    .then(function(json) { return fireCache = modifyFeatures(json, setFireStyle); })//calcBounds(json); })
+    .then(function(json) { return fireCache = modifyFeatures(json, setFireStyle, fireCache); })//calcBounds(json); })
     .then(function(json) { fs.writeFile(firemoddest, JSON.stringify(fireCache), function (err) {
           if (err) return console.log(err);
           else return json;
-        }) })
+        }) })*/
 
   if (typeof res !== "undefined") res.sendStatus("<h1>" + FIREREPLY + "</h1>")
 }//updateFireData
